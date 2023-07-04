bb1b9424ee22369db65d476f21b49088993d0e42
Vaidas <mano@mailas.lt>
2023-07-04 11:43:39 +0300

    Added MockBean to HomeControllerTests

--- src/main/java/lt/codeacademy/javau5/tdd/HomeController.java
+++ src/main/java/lt/codeacademy/javau5/tdd/HomeController.java
@@ -1,11 +1,15 @@
 package lt.codeacademy.javau5.tdd;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+	@Autowired
+	MainService service;
+	
 	@GetMapping("/daiktas")
 	public Daiktas getDaiktas() {
 
--- src/test/java/lt/codeacademy/javau5/tdd/HomeControllerTests.java
+++ src/test/java/lt/codeacademy/javau5/tdd/HomeControllerTests.java
@@ -7,6 +7,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(HomeController.class)
@@ -14,6 +15,9 @@ public class HomeControllerTests {
 	
 	@Autowired
 	private MockMvc mockMvc;
+	
+	@MockBean
+	private MainService mainService;
 	
 	@Test
 	public void testGetDaiktasEndpoint() throws Exception {		
